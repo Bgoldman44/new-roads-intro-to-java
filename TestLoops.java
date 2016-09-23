a7f13abf9e6672eb79e648dcef98e9a8b298c093
bengoldman44 <bengoldman44@yahoo.com>
2016-09-23 11:54:29 -0400

    loops

---
+++ TestLoops.java
@@ -0,0 +1,13 @@
+public class TestLoops{
+
+ public static void main(String[] args){
+ 
+  int x = 2;
+   
+   for(int index = 0; index < 10; index+=2){
+     
+     System.out.println(x+index);
+   }
+       
+   }
+ }
